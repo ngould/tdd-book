f930c2b4b2e882cc97cf23137a9f8d7b05bbbc6f
nathan <nathan.gould@gmail.com>
2015-01-19 01:06:05 -0500

    functional tests now check that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,24 +18,29 @@ class NewVisitorTest(unittest.TestCase):
     self.browser.get('http://localhost:8000')
 
     # She notices that the title says to-do's
-    assert 'To-Do' in self.browser.title
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the the test!')
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
     # She is invited to enter a to-do item
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+        inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-
-    # She types "Buy peackock feathers" in the text box
-
+    # She types "Buy peacock feathers" in the text box
+    inputbox.send_keys('Buy peacock feathers')
 
     # When she hits enter, the page updates and now she sees
-    # "Buy peackock feathers" in her list of to-do's
-
-
+    # "Buy peacock feathers" in her list of to-do's
+    inputbox.send_keys(Keys.ENTER)
+    
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
     # The text box is still there, and Edith enters another
-    # to-do: "Use peackock feathers". 
-
+    # to-do: "Use peacock feathers". 
+    self.fail('Finish the test!')
 
     #She sees that on her list once th page updates.
 
