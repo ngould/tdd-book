ce98a9d0b2d34fc0d3f40ebea56956ecd2694a2d
nathan <nathan.gould@gmail.com>
2015-01-20 01:22:47 -0500

    urls for forms, moved code in views and tests, new url

--- lists/tests.py
+++ lists/tests.py
@@ -40,32 +40,24 @@ class HomePageTest(TestCase):
     
     self.assertEqual(response.content.decode(), expected_html)
 
-  def test_home_page_can_save_a_POST_request(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
 
-    response = home_page(request)
+class NewListTest(TestCase):
 
+  def test_saving_a_POST_request(self):
+    self.client.post(
+      '/lists/new',
+      data={'item_text': 'A new list item'}
+    )
     self.assertEqual(Item.objects.count(), 1)
     new_item = Item.objects.first()
     self.assertEqual(new_item.text, 'A new list item')
 
-  def test_home_page_redirects_after_post(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
-
-    response = home_page(request)
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-  def test_homepage_only_saves_items_when_necessary(self):
-    request = HttpRequest()
-    home_page(request)
-    items_after = Item.objects.all().count()
-    self.assertEquals(items_after, 0)
+  def test_redirects_after_post(self):
+    response = self.client.post(
+      '/lists/new',
+      data={'item_text': 'A new list item'}
+    )
+    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ListViewTest(TestCase):
