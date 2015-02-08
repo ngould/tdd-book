860c0609714f1dc1b09c35a63fb860a67d6878bd
nathan <nathan.gould@gmail.com>
2015-02-08 15:01:38 -0500

    new url and view for adding to existing lists. FT passes.

--- lists/migrations/0004_item_list.py
+++ lists/migrations/0004_item_list.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.ForeignKey(default=None, to='lists.List'),
+            field=models.ForeignKey(default=None, to='lists.List', null=True),
             preserve_default=True,
         ),
     ]
