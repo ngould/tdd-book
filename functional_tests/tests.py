2cd38dd1b0afcc3b69df950b670200fd26f5dec4
nathan <nathan.gould@gmail.com>
2015-02-09 01:32:15 -0500

    configured staticfiles

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
@@ -86,6 +86,28 @@ class NewVisitorTest(LiveServerTestCase):
 
     # Satisfied, they both go to sleep
 
+  def test_layout_and_styling(self):
+    #Edith goes to the home page
+    self.browser.get(self.live_server_url)
+    self.browser.set_window_size(1024, 768)
+
+    # She notices the input box is nicely centered
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width'] / 2,
+      512,
+      delta=5,
+    )
+
+    # She starts a new list and sees the input is nicely
+    # centered there too
+    inputbox.send_keys('testing\n')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width'] / 2,
+      512,
+      delta=5,
+    )
 
 
 
